eacfacdf16918613e82a7df0bf3abe6f01a445c6
Stav-Sananes <stav.s2020@gmail.com>
2026-05-29 15:41:19 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
 #include <string>
-#include <format>
 
-int main()
-{
+int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
   std::string command;
-
+  
   std::cout << "$ ";
   std::cin >> command;
-  if (command == "xyz")
-  {
-    std::string result = std::format("command not found: {}", command);
-    std::cout << result << std::endl;
-  }
+  std::cout << "{command}: command not found" << command << std::endl;
 }
