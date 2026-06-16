c330be8e7c938815d614a651d4ed28170cfa2ae9
Stav-Sananes <stav.s2020@gmail.com>
2026-06-16 20:54:46 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
+namespace fs = std::filesystem;
+bool has_execute_permission(const fs::path &file_path)
+{
+  if (!fs::exists(file_path))
+  {
+    return false;
+  }
 
+  fs::perms p = fs::status(file_path).permissions();
+
+  return (p & (fs::perms::owner_exec | fs::perms::group_exec | fs::perms::others_exec)) != fs::perms::none;
+}
 int main()
 {
   // Flush after every std::cout / std:cerr
@@ -19,17 +31,25 @@ int main()
     else if (command.substr(0, 5) == "echo ")
     {
       std::cout << command.substr(5) << std::endl;
-    } else if(command.substr(0, 4) == "type"){
-      if(command.substr(5) == "echo" || command.substr(5) == "exit" || command.substr(5) == "type"){
+    }
+    else if (command.substr(0, 4) == "type")
+    {
+      if (command.substr(5) == "echo" || command.substr(5) == "exit")
+      {
         std::cout << command.substr(5) << " is a shell builtin" << std::endl;
-      } else {
-         std::cout << command.substr(5) << ": not found" << std::endl;
+      }
+      else if ((command.substr(5) == "grep" || command.substr(5) == "ls") && has_execute_permission("/usr/bin/" + command.substr(5)))
+      {
+        std::cout << command.substr(5) << " is " << fs::current_path() << std::endl;
+      }
+      else
+      {
+        std::cout << command.substr(5) << ": not found" << std::endl;
       }
     }
     else
     {
       std::cout << command << ": command not found" << std::endl;
     }
-    
   }
 }
