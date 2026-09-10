6594adca383801348f110344487bb07604a29bfe
Stav-Sananes <stav.s2020@gmail.com>
2026-09-10 23:48:19 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -111,8 +111,8 @@ int main()
       if (arg.size() >= 2 && arg.front() == '"' && arg.back() == '"')
         arg = arg.substr(1, arg.size() - 2);
       std::filesystem::path new_path = arg;
-      if(new_path == "~"){
-         new_path = std::filesystem::path(getenv("HOME"));
+      if(new_path == '~'){
+         new_path = std::filesystem::current_path(getenv("HOME"));
       }
       else if (!std::filesystem::exists(new_path))
       {
